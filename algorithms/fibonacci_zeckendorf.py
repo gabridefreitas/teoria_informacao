fb7eba98b3b503cfb389210bc84e62ba783899bd
Gabriel de Freitas <contat.gabrieldefreitas@gmail.com>
2024-10-07 06:46:44 -0300

    refactor: :recycle: lint

--- algorithms/fibonacci_zeckendorf.py
+++ algorithms/fibonacci_zeckendorf.py
@@ -1,25 +1,16 @@
-
-
-
-
-
-def encode_fibonacci_zeckendorf(string):
-    ret=""
-    for char in string:
-        ret+=codify_char(char)+"1" #sera adicionado o stopbit aqui entao a = 000100011
-    return ret
-
-
-def codify_char(char): #exemplo 10001000 = a
+def codify_char(char):
     n = ord(char)
     result = []
     fibonacci = [1, 2]
+
     while True:
         next_fib = fibonacci[-1] + fibonacci[-2]
         if next_fib > n:
             break
         fibonacci.append(next_fib)
+
     first_one = False
+
     for i in range(len(fibonacci) - 1, -1, -1):
         if n >= fibonacci[i]:
             result.append("1")
@@ -27,43 +18,54 @@ def codify_char(char): #exemplo 10001000 = a
             first_one = True
         elif first_one:
             result.append("0")
+
     if not result:
         return "0"
 
-    string_prep="".join(result)
-    
-    return "".join(result)[::-1] #retornara 00010001
+    return "".join(result)[::-1]
 
 
-def decode_fibonacci_zeckendorf(code):
-    ret=""
-    current_index = 0
-    
-    while current_index < len(code):
-        end_index = code.find('11', current_index) #acha o index do primeiro 11
-    
-        if end_index == -1:
-            return ret;
-        end_index+=1 #compensa o index pra pegar o 1 faltante
-        string_code = code[current_index:end_index]
-        ret+=chr(decode_char(string_code))
-        current_index=end_index+1 #compensa o index para pegar o proximo codigo
+def encode_fibonacci_zeckendorf(input_string):
+    ret = ""
+
+    for char in input_string:
+        ret += codify_char(char) + "1"
 
     return ret
 
 
-
-
-
 def decode_char(code):
-    code = code[::-1] #reverte o codigo para poder decodar ie. 00010001 vai virar 10001000
+    code = code[::-1]
+
     result = 0
     fib = [0, 1]
+
     while len(fib) < len(code) + 2:
         next_fib = fib[-1] + fib[-2]
         fib.append(next_fib)
+
     for i in range(len(code)):
         if code[len(code) - 1 - i] == "1":
             result += fib[i + 2]
+
     return result
 
+
+def decode_fibonacci_zeckendorf(input_string):
+    ret = ""
+    current_index = 0
+
+    while current_index < len(input_string):
+        end_index = input_string.find("11", current_index)
+
+        if end_index == -1:
+            return ret
+
+        end_index += 1
+
+        string_code = input_string[current_index:end_index]
+        ret += chr(decode_char(string_code))
+
+        current_index = end_index + 1
+
+    return ret
